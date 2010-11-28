4f4f1111dae96fa6682cd35912521af0e1b9429b
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2010-11-28 15:06:59 +0100

    Log4J modules are configurables Multiple modules can coexists in the same time

--- log-handler-log4j/src/main/java/org/ow2/ipojo/toolkit/log/internal/log4j/Log4JLoggingModule.java
+++ log-handler-log4j/src/main/java/org/ow2/ipojo/toolkit/log/internal/log4j/Log4JLoggingModule.java
@@ -1,10 +1,19 @@
 package org.ow2.ipojo.toolkit.log.internal.log4j;
 
+import org.apache.felix.ipojo.annotations.Property;
 import org.apache.felix.ipojo.annotations.ServiceProperty;
 import org.apache.felix.ipojo.annotations.Validate;
+import org.apache.log4j.Hierarchy;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.spi.LoggerRepository;
+import org.apache.log4j.spi.RootLogger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.osgi.framework.BundleContext;
 import org.ow2.ipojo.toolkit.log.spi.LoggingModule;
+
+import java.net.URL;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,9 +27,28 @@ public class Log4JLoggingModule implements LoggingModule {
     @ServiceProperty(mandatory = true, value = ".*")
     private String pattern;
 
+    @Property(name = "resource", mandatory = true)
+    private String resourceName;
+
+    private BundleContext bundleContext;
+    private LoggerRepository repository;
+
+    public Log4JLoggingModule(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
     @Validate
     private void configure() {
-        //LoggerFactory
+        RootLogger root = new RootLogger(Level.DEBUG);
+        repository = new Hierarchy(root);
+
+        URL resource = bundleContext.getBundle().getResource(resourceName);
+
+        if (resourceName.endsWith(".xml")) {
+            new DOMConfigurator().doConfigure(resource, repository);
+        } else {
+            new PropertyConfigurator().doConfigure(resource, repository);
+        }
     }
 
     public <T> T getLogger(Class<T> expected, String name) {
@@ -29,7 +57,7 @@ public class Log4JLoggingModule implements LoggingModule {
             return null;
         }
 
-        Logger logger = Logger.getLogger(name);
+        Logger logger = repository.getLogger(name);
         return expected.cast(logger);
     }
 }
